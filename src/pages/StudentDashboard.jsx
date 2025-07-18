13a773b92daace83f5d6dbc14196b07f89ee018e
rickypanu <rickypanu2005@gmail.com>
2025-07-19 00:38:07 +0530

    subject

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -6,23 +6,37 @@ import { getFingerprint } from "../services/getFingerprint"
 import {LogOut,} from "lucide-react";
 
 const SUBJECTS = {
-  CSE: {
-    1: ["Maths", "Physics"],
-    2: ["DSA", "OOP"],
-    3: ["DBMS", "OS"],
-    5: ["Computer Graphics", "Theory of Computation", "Artificial Intelligence", "Natural language Processing","Economics"],
-  },
-  ECE: {
-    1: ["Basic Electronics", "Maths"],
-    2: ["EMT", "Digital"],
-    3: ["DSP", "VLSI"],
-    5: ["VLSI", "AWP","DSD","DSA", "DSP", "CN"],
-  },
-  MECH: {
-    1: ["Mechanics", "Maths"],
-    2: ["Thermodynamics", "Fluid Mechanics"],
-  },
-};
+    "CSE": {
+        "1": ["Calculus", "Physics", "Professional Communication","Workshop" , "Programming Fundamentals", "UHV"],
+        "2": ["Basic Electrical and Electronics Engineering", "Applied Chemistry", "Engineering Graphics", "OOPs", "Differential Equations and Transforms"],
+        "3": ["DBMS", "Data Structure", "Discrete Systems", "Web Technologies", "Software Technologies"],
+        "4": [""],
+        "5": ["Computer Graphics", "Theory of Computation", "Artificial Intelligence", "Natural language Processing","Economics"],
+        "6": [""],
+        "7": [""],
+        "8": [""],
+    },
+    "ECE": {
+        "1": ["Basic Electrical and Electronics Engineering", "Engineering Graphics", "Applied Chemistry", "Calculus", "Programming Fundamentals", ],
+        "2": ["Workshop", "Digital Design", "Professional Communication", "UHV", "Applies Physics"],
+        "3": ["Linear Algebra and Complex Analysis", "Signals and Systems", "Microprocessor and Microcontroller", "Electronic Devices and Circuits", "Electronics Measurementsand Instrumentation", "Economics"],
+        "4": ["Communication Engineering", "Advance Microcontroller and Application", "Analog Electronics Circuits", "Probability and Random Process", "Electromagnetic Theory", "Network Analysis"],
+        "5": ["VLSI", "AWP","DSD","DSA", "DSP", "CN"],
+        "6": ["Microwave & Radar Engineering", "Fibre Optic Communication Systems", "Digital Communication", "Control Systems", "Power Electronics", "Satellite Communication" ],
+        "7": ["Wireless and Mobile Communication", "Embedded System Design"],
+        "8": [""],
+    },
+    "MECH": {
+        "1": ["Mechanics", "Maths"],
+        "2": ["Thermodynamics", "Fluid Mechanics"],
+        "3": [""],
+        "4": [""],
+        "5": [""],
+        "6": [""],
+        "7": [""],
+        "8": [""],
+    },
+}
 
 export default function StudentDashboard() {
   const [otp, setOtp] = useState("");
