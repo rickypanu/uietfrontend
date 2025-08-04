4e5b4bff066972cec2d4e9bbe4263dcdc3629263
rickypanu <rickypanu2005@gmail.com>
2025-08-04 09:29:49 +0530

    msg

--- src/pages/StudentDashboard.jsx
+++ src/pages/StudentDashboard.jsx
@@ -314,7 +314,8 @@ export default function StudentDashboard() {
         )}
         {otpError && <p className="text-red-600">{otpError}</p>}
         {message && (
-          <p className={`text-sm ${message.startsWith('✅') ? 'text-green-700' : 'text-red-600'}`}>
+          // <p className={`text-sm ${message.startsWith('✅') ? 'text-green-700' : 'text-red-600'}`}>
+            <p className={`text-sm ${message.startsWith('') ? 'text-green-700' : 'text-red-600'}`}></p>
             {message}
           </p>
         )}
