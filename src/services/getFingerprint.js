aece2d16ae58918b5d9f85d7b98159eb2c0a46da
rickypanu <rickypanu2005@gmail.com>
2025-08-15 00:55:04 +0530

    device fingerprint

--- src/services/getFingerprint.js
+++ src/services/getFingerprint.js
@@ -1,10 +1,24 @@
 // src/utils/getFingerprint.js
+// import FingerprintJS from '@fingerprintjs/fingerprintjs';
+
+// export async function getFingerprint() {
+//   // Initialize the agent at application startup.
+//   const fp = await FingerprintJS.load();
+//   // Get the visitor identifier when you need it.
+//   const result = await fp.get();
+//   return result.visitorId; // this is the unique device fingerprint
+// }
+
 import FingerprintJS from '@fingerprintjs/fingerprintjs';
 
 export async function getFingerprint() {
-  // Initialize the agent at application startup.
+  let storedId = localStorage.getItem("visitorId");
+  if (storedId) {
+    return storedId;
+  }
+
   const fp = await FingerprintJS.load();
-  // Get the visitor identifier when you need it.
   const result = await fp.get();
-  return result.visitorId; // this is the unique device fingerprint
+  localStorage.setItem("visitorId", result.visitorId);
+  return result.visitorId;
 }
