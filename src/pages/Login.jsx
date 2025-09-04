14e6320ef9e1653c8421da199a1a674248cf87c5
rickypanu <rickypanu2005@gmail.com>
2025-09-04 20:14:35 +0530

    dob

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -94,21 +94,18 @@ export default function Login() {
               placeholder={role === "student" ? "Roll Number" : "Employee ID"}
               className="w-full border border-gray-300 rounded-lg px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
             />
+
           </div>
 
           <div>
             <label className="block text-gray-700 text-sm mb-1">Password (DOB)</label>
             <input
               required
-              type="text"              // Use text instead of date
-              inputMode="numeric"      // Trigger numeric keyboard
-              pattern="\d{4}-\d{2}-\d{2}" // Optional: enforce YYYY-MM-DD format
-              placeholder="YYYY-MM-DD"
-              defaultValue={dob}
+              type="date"
+              value={dob}
               onChange={(e) => setDob(e.target.value)}
               className="w-full border border-gray-300 rounded-lg px-3 py-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400"
             />
-
           </div>
 
           <button
